a7b13fdfd8fbbfc2b509cfac7e6010acdab25807
Linzhen (Brian) Zhu <146813475+LinzhenZHU@users.noreply.github.com>
2024-11-11 15:29:58 -0500

    Update Wearable_Vibration_Terminal.ino

--- Wearable_Vibration_Terminal.ino
+++ Wearable_Vibration_Terminal.ino
@@ -1,4 +1,4 @@
-int Pin = D10;  // connected to digital pin 9
+int Pin = D10;  // connected to digital pin 
 
 void setup() {
 
@@ -6,7 +6,7 @@ void setup() {
 
 void loop() {
   // fade in from min to max in increments of 5 points:
-  for (int fadeValue = 255; fadeValue <= 255; fadeValue += 5) {
+  for (int fadeValue = 0; fadeValue <= 255; fadeValue += 5) {
     // sets the value (range from 0 to 255):
     analogWrite(Pin, fadeValue);
     // wait for 30 milliseconds to see the dimming effect
@@ -14,7 +14,7 @@ void loop() {
   }
 
   // fade out from max to min in increments of 5 points:
-  for (int fadeValue = 0; fadeValue >= 0; fadeValue -= 5) {
+  for (int fadeValue = 255; fadeValue >= 0; fadeValue -= 5) {
     // sets the value (range from 0 to 255):
     analogWrite(Pin, fadeValue);
     // wait for 30 milliseconds to see the dimming effect
